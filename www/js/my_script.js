c74720469900f26660b83bce009685970c275900
Aaron Roles <aaron_roles93@hotmail.com>
2017-08-02 12:16:20 +0100

    next commit

--- www/js/my_script.js
+++ www/js/my_script.js
@@ -3,12 +3,26 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
     var url = "http://webhq.ie/wp-json/wp/v2/posts";
-    $.getJSON(url, function (result) {
-        $.each(result.posts, function (i, response) {
-            var title = response.title;
-            var content = response.content;
-            var date = response.date;
-            $("#listview").append("<li><h3>" + title + "</h3><div>" + content + date + "</div></li>");
-        });
+
+    jQuery.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'json',
+        success: function(data){
+            jQuery.each(data, function(index, value) {
+                console.log(value);
+                jQuery('#listview').append('<li>' +
+                    '<h3>Post Title: '+ value.title.rendered +'</h3>' +
+                    '<p>Post Content: '+ value.content.rendered +'</p></li>');
+
+            });
+        }
     });
 }
+
+// $.getJSON(url, function (result) {
+    //     alert(result)
+    //     $.each(result.posts, function (i, response) {    
+    //         $("#listview").append("<li>" + response + "</li>");
+    //     });
+    // });
