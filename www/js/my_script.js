365e48b1c157db123560aff5d419f4174e27b754
Aaron Roles <aaron_roles93@hotmail.com>
2017-08-02 14:45:18 +0100

    getting posts, pages and cpt

--- www/js/my_script.js
+++ www/js/my_script.js
@@ -2,28 +2,4 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 
-    var url = "https://webhq.ie/wp-json/wp/v2/posts";
-
-    jQuery.ajax({
-        type: 'GET',
-        url: url,
-        dataType: 'json',
-        crossDomain: true,
-        success: function(data){
-            jQuery.each(data, function(index, value) {
-                console.log(value);
-                jQuery('#listview').append('<li>' +
-                    '<h3>Post Title: '+ value.title.rendered +'</h3>' +
-                    '<p>Post Content: '+ value.content.rendered +'</p></li>');
-
-            });
-        }
-    });
 }
-
-// $.getJSON(url, function (result) {
-    //     alert(result)
-    //     $.each(result.posts, function (i, response) {    
-    //         $("#listview").append("<li>" + response + "</li>");
-    //     });
-    // });
