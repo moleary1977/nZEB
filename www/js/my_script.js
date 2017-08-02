9ddbe90be402a835dca278aab791115b198dc16f
Aaron Roles <aaron_roles93@hotmail.com>
2017-08-02 11:26:03 +0100

    commit 2

--- www/js/my_script.js
+++ www/js/my_script.js
@@ -1,4 +1,9 @@
-$(document).ready(function(){
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady(){
+    
+    checkConnection();
+
     var url="http://www.webhq.ie/?json=get_recent_posts";
     $.getJSON(url,function(result){
         $.each(result.posts, function(i, response){
@@ -8,4 +13,20 @@ $(document).ready(function(){
         $("#listview").append("<li><h3>"+title+"</h3><div>"+content+date+"</div></li>");
         });
     });
-});
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
