d8d443723609da1b103611c3b13d36e4659e6045
Aaron Roles <aaron_roles93@hotmail.com>
2017-08-03 11:44:57 +0100

    multi-languages

--- platforms/browser/www/js/my_script.js
+++ platforms/browser/www/js/my_script.js
@@ -4,8 +4,10 @@ document.addEventListener("deviceready", onDeviceReady, false);
 * This function is called when the device is ready
 */
 function onDeviceReady() {
-    if(getLanguage){
+    if(getLanguage != null){
         jQuery("#activeLang").text(getLanguage);
+    } else {
+        jQuery("#activeLang").text("No language selected");
     }
 }
 
