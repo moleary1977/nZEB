e4fe6f0243f4be5e274b7ae388321e133c89a5a6
Aaron Roles <aaron_roles93@hotmail.com>
2017-08-03 11:28:35 +0100

    adding multiple languages

--- www/js/my_script.js
+++ www/js/my_script.js
@@ -1,5 +1,25 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
+/*
+* This function is called when the device is ready
+*/
 function onDeviceReady() {
+    if(getLanguage){
+        jQuery("#activeLang").text(getLanguage);
+    }
+}
 
+/*
+ * This function takes in a parameter to set the user's language and store it locally
+ */
+function setLanguage(language){
+    localStorage.setItem("user_language", language);
+    jQuery("#activeLang").text(language);
+}
+
+/*
+ * This function returns the language stored locally
+ */
+function getLanguage(){
+    return localStorage.getItem("user_language");
 }
