0d7162326ff9fb94d7b26626fce5aaf872dd7d59
Aaron Roles <aaron_roles93@hotmail.com>
2017-08-08 12:12:40 +0100

    prototyping training questions

--- www/js/my_script.js
+++ www/js/my_script.js
@@ -4,7 +4,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
 * This function is called when the device is ready
 */
 function onDeviceReady() {
-    if(getLanguage != null){
+
+    if (getLanguage != null) {
         jQuery("#activeLang").text(getLanguage);
     } else {
         jQuery("#activeLang").text("No language selected");
@@ -14,7 +15,7 @@ function onDeviceReady() {
 /*
  * This function takes in a parameter to set the user's language and store it locally
  */
-function setLanguage(language){
+function setLanguage(language) {
     localStorage.setItem("user_language", language);
     jQuery("#activeLang").text(language);
 }
@@ -22,6 +23,30 @@ function setLanguage(language){
 /*
  * This function returns the language stored locally
  */
-function getLanguage(){
+function getLanguage() {
     return localStorage.getItem("user_language");
+}
+
+function login() {
+    var username = jQuery('#username').val();
+    var password = jQuery('#password').val();
+
+    var url = "http://www.webhq.ie/wp-admin/admin-ajax.php";
+
+    jQuery.ajax({
+        type: 'POST',
+        url: url,
+        data: { action: 'login', username: username, password: password },
+        crossDomain: true,
+        success: function (data, status, xhr) {
+            alert(data);
+            alert(status);
+            alert(xhr.status);
+        },
+        error: function (xhr, status, errorThrown) {
+            alert(xhr.status);
+            alert(status);
+            alert(errorThrown);
+        }
+    });
 }
