cad8cc4debee865a116fa0bd3ca3e8e178fcccc3
Aaron Roles <aaron_roles93@hotmail.com>
2017-08-23 10:49:43 +0100

    Added home page design

--- www/js/functions.js
+++ www/js/functions.js
@@ -175,3 +175,11 @@ function saveResults() {
         }
     });
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
