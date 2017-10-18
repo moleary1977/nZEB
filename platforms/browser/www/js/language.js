8862f9ae1d31c1f307acd1d81c7b0e006a31f824
Aaron Roles <aaron_roles93@hotmail.com>
2017-10-18 17:19:44 +0100

    Adding language packages

---
+++ platforms/browser/www/js/language.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function () {
+
+    // if the cookie exists, the user is logged in
+    if (document.cookie) {
+        lang = getLanguage();
+        url = '../res/json/language.json';
+        // get the json data from the url
+        jQuery.getJSON(url, function(json){
+            // loop through the json
+            jQuery.each(json, function (language_pack, pack) {
+                // If the user's language matches a language package
+                if (language_pack === lang) {
+                    // loop through pack
+                    jQuery.each(pack, function(element, text){
+
+                        // These menu items will always exist
+                        jQuery("#menu_home").append(pack[element].menu_home);
+                        jQuery("#logout").append(pack[element].menu_logout);
+                        jQuery('.menu_about').append(pack[element].menu_about);
+                        jQuery('.menu_training').append(pack[element].menu_training);
+                        jQuery('.menu_search').append(pack[element].menu_search);
+                        jQuery('.menu_links').append(pack[element].menu_links);
+                        
+                        // If homepage with menu subtitles
+                        if (jQuery('#subtitles_on_page').length) {                            
+                            jQuery('#subtitle_about').append(pack[element].subtitle_about);
+                            jQuery('#subtitle_training').append(pack[element].subtitle_training);
+                            jQuery('#subtitle_search').append(pack[element].subtitle_search);
+                            jQuery('#subtitle_links').append(pack[element].subtitle_links);
+                            if(getCoursesCompleted() == 0){
+                                jQuery("#menu_bottom a").append(pack[element].menu_bottom_start);
+                            } else {
+                                jQuery("#menu_bottom a").append(pack[element].menu_bottom_continue);
+                            }
+                        }
+
+                        // If profile page
+
+                        // If search page
+
+                        // If training page
+                    });
+                }
+            })
+        });
+    }
+
+});
